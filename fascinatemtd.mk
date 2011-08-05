b35191dec0549edd607a5f8cbad28f7e2a362194
jt1134 <jt1134@gmail.com>
2011-08-04 19:47:54 -0500

    update prebuilt kernel and modules also fixes for wifi tether, ota, and volume steps
    Change-Id: I500fc1845fa95cd55e78a73e670c0e087a8d1362

--- fascinatemtd.mk
+++ fascinatemtd.mk
@@ -131,7 +131,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
        ro.cdma.home.operator.alpha=Verizon \
        ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
        ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
-       ro.cdma.otaspnumschema=SELC,1,80,99 \
+       ro.cdma.otaspnumschema=SELC,3,00,07,80,87,88,99 \
+       ro.config.vc_call_vol_steps=15 \
        ro.telephony.call_ring.multiple=false \
        ro.telephony.call_ring.delay=3000 \
        ro.telephony.call_ring.absent=true \
