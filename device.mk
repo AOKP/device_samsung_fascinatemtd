0ca3ad3ce834587819c3ba10b584c303dd82c9ae
jt1134 <jt1134@gmail.com>
2012-08-25 03:51:04 -0500

    clean up keymaps
    Change-Id: I2b5c44c95456b2d1fc2bdcf03618616748210f4e

--- device.mk
+++ device.mk
@@ -56,9 +56,8 @@ PRODUCT_COPY_FILES += \
 
 # Prebuilt kl keymaps
 PRODUCT_COPY_FILES += \
-	device/samsung/fascinatemtd/cypress-touchkey.kl:system/usr/keylayout/cypress-touchkey.kl \
+	device/samsung/aries-common/cypress-touchkey.kl:system/usr/keylayout/cypress-touchkey.kl \
 	device/samsung/aries-common/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
-	device/samsung/fascinatemtd/aries-keypad.kl:system/usr/keylayout/aries-keypad.kl \
 	device/samsung/aries-common/s3c-keypad.kl:system/usr/keylayout/s3c-keypad.kl
 
 # ppp
