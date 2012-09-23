544313efc389e9e7c09ee7bc84d6fa9537bf0ff0
Stevespear426 <stephen.k.spear@gmail.com>
2012-09-23 08:35:57 -0400

    Fix getting wrong apns-conf by default
    This fixes the MMS issue fascinate was having.
    
    Change-Id: I177f5d439a2b9a6d2cb3d90e158ddd58c9d05d9a
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

--- device.mk
+++ device.mk
@@ -106,7 +106,8 @@ PRODUCT_PACKAGES += \
 	sensors.aries
 
 PRODUCT_COPY_FILES += \
-	device/samsung/fascinatemtd/libaudio/audio_policy.conf:system/etc/audio_policy.conf
+    device/samsung/fascinatemtd/libaudio/audio_policy.conf:system/etc/audio_policy.conf \
+    vendor/aokp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # Libs
 PRODUCT_PACKAGES += \
